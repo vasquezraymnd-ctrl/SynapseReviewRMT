816894fa98d7910c62f97acfbac7c99a7cce4a7d
vasquez.raymnd <vasquez.raymnd@gmail.com>
2026-03-19 06:07:06 +0000

    upload security

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZmSBGlY5Pza_mXIhFjl9fj0SDXeO8OJE",
-  authDomain: "studio-4582615362-8c3e1.firebaseapp.com",
-  projectId: "studio-4582615362-8c3e1",
-  storageBucket: "studio-4582615362-8c3e1.firebasestorage.app",
-  messagingSenderId: "100300675060",
-  appId: "1:100300675060:web:4d1947e271031d41a04ba1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
