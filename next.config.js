93218dbb77da08e8cf92a1edb50d95cc091abe42
vasquez.raymnd <vasquez.raymnd@gmail.com>
2026-03-19 08:53:15 +0000

    lok

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverActions: {
-    bodySizeLimit: '50mb', // Increase the body size limit to 50MB
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
